400afbabc562eccce90d8f558dc0bc6c513fa8da
wangxizzz <979675820@qq.com>
2018-12-02 22:16:52 +0800

    jdk1.8中time包介绍

--- src/main/java/java8/time/DateTest.java
+++ src/main/java/java8/time/DateTest.java
@@ -1,21 +1,17 @@
-package java8;
+package java8.time;
 
 import java.text.ParseException;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 
-/***************************************
- * @author:Alex Wang
- * @Date:2016/11/13 QQ:532500648
- * QQ交流群:286081824
- ***************************************/
+/**
+ * 日期date与时间time是分开的
+ */
 public class DateTest {
     public static void main(String[] args) throws ParseException, InterruptedException {
-/*        Date date = new Date(116, 2, 18);
-        System.out.println(date);*/
-
-/*        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        /* // 多线程会解析异常 sdf是30个线程共享变量
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         for (int i = 0; i < 30; i++) {
             new Thread(() -> {
                 for (int x = 0; x < 100; x++) {
@@ -41,18 +37,21 @@ public class DateTest {
     }
 
     private static void testLocalDate() {
+        // 自定义日期
         LocalDate localDate = LocalDate.of(2016, 11, 13);
         System.out.println(localDate.getYear());
         System.out.println(localDate.getMonth());
         System.out.println(localDate.getMonthValue());
         System.out.println(localDate.getDayOfYear());
         System.out.println(localDate.getDayOfMonth());
+        // 得到这一周的哪一天
         System.out.println(localDate.getDayOfWeek());
 
         localDate.get(ChronoField.DAY_OF_MONTH);
     }
 
     private static void testLocalTime() {
+        // 可以拿到当前的时间
         LocalTime time = LocalTime.now();
         System.out.println(time.getHour());
         System.out.println(time.getMinute());
@@ -60,7 +59,9 @@ public class DateTest {
     }
 
     private static void combineLocalDateAndTime() {
+        // 获取当前日期
         LocalDate localDate = LocalDate.now();
+        // 获取当前时间
         LocalTime time = LocalTime.now();
 
         LocalDateTime localDateTime = LocalDateTime.of(localDate, time);
@@ -69,28 +70,37 @@ public class DateTest {
         System.out.println(now);
     }
 
+    /**
+     * Instant表示一个时间点，当前瞬间。
+     */
     private static void testInstant() throws InterruptedException {
         Instant start = Instant.now();
         Thread.sleep(1000L);
         Instant end = Instant.now();
+        // Duration代表一个时间的片段，表示跨了多少时间
         Duration duration = Duration.between(start, end);
         System.out.println(duration.toMillis());
     }
 
     private static void testDuration() {
         LocalTime time = LocalTime.now();
+        // minusHours 表示subtracted减掉多长时间
         LocalTime beforeTime = time.minusHours(1);
         Duration duration = Duration.between(time, beforeTime);
         System.out.println(duration.toHours());
     }
 
     private static void testPeriod() {
+        // Period表示时间跨度比较大。
         Period period = Period.between(LocalDate.of(2014, 1, 10), LocalDate.of(2016, 1, 10));
         System.out.println(period.getMonths());
         System.out.println(period.getDays());
         System.out.println(period.getYears());
     }
 
+    /**
+     * 格式化日期
+     */
     private static void testDateFormat() {
         LocalDate localDate = LocalDate.now();
         String format1 = localDate.format(DateTimeFormatter.BASIC_ISO_DATE);
@@ -103,6 +113,9 @@ public class DateTest {
         System.out.println(format);
     }
 
+    /**
+     * 解析时间
+     */
     private static void testDateParse() {
         String date1 = "20161113";
         LocalDate localDate = LocalDate.parse(date1, DateTimeFormatter.BASIC_ISO_DATE);
