657de352de8c5335d728702ea0265052e2711435
wxi.wang <wxi.wang@qunar.com>
2019-10-08 20:19:26 +0800

    guava cache使用

--- src/main/java/常用的工具类/guava/cache/Demo01.java
+++ src/main/java/常用的工具类/guava/cache/Demo01.java
@@ -5,17 +5,22 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalListeners;
 import com.google.common.cache.RemovalNotification;
 import com.google.common.cache.Weigher;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by wxi.wang
- * <p> 参考网址：https://www.jianshu.com/p/5299f5a11bd5
+ * <p>
+ *  参考网址：
+ *       https://www.jianshu.com/p/5299f5a11bd5
+ *       https://www.jianshu.com/p/b51fc9ed0e71
  * 2019/10/8 17:30
  * Decription:
  */
@@ -34,6 +39,7 @@ public class Demo01 {
     /**
      * 方式1：缓存没命中
      * 没有缓存，那么计算，然后再缓存，并且返回计算的值
+     * 此方式是key和value存在关系
      */
     @Test
     public void test02() {
@@ -103,6 +109,7 @@ public class Demo01 {
         // 设置缓存最大重量为maximumWeight 为100，如果缓存的重量快要到达100，那么就开始回收
         Cache<Integer, Integer> cache = CacheBuilder.newBuilder().maximumWeight(100)
                 .weigher(new Weigher<Integer, Integer>() {
+                    // 注意需要考虑计算缓存重量的复杂度，重量是在缓存创建时计算
                     @Override
                     public int weigh(Integer key, Integer value) {
                         if (value % 2 == 0) {
@@ -205,12 +212,16 @@ public class Demo01 {
 
     /**
      * 添加移除监听器
+     * 默认情况下，监听器方法是在移除缓存时同步调用的。因为缓存的维护和请求响应通常是同时进行的，
+     * 代价高昂的监听器方法在同步模式下会拖慢正常的缓存请求。
+     * 在这种情况下，你可以使用RemovalListeners.asynchronous(RemovalListener, Executor)把监听器装饰为异步操作。
      */
     @Test
     public void test10() {
         LoadingCache<Integer, Integer> cache = CacheBuilder.newBuilder()
                 .expireAfterWrite(3, TimeUnit.SECONDS)
-                .removalListener(new RemovalListener<Object, Object>() {
+
+                .removalListener(RemovalListeners.asynchronous(new RemovalListener<Object, Object>() {
                     // 当缓存被移除时执行
                     @Override
                     public void onRemoval(RemovalNotification<Object, Object> notification) {
@@ -218,7 +229,9 @@ public class Demo01 {
                                 "],value[" + notification.getValue() + "],remove reason[" +
                                 notification.getCause() + "]");
                     }
-                }).recordStats().build(
+                }, Executors.newSingleThreadExecutor()))
+                .recordStats()  // 记载统计信息
+                .build(
                         // 缓存key不存在时，默认返回的值
                         new CacheLoader<Integer, Integer>() {
                             @Override
@@ -227,6 +240,8 @@ public class Demo01 {
                             }
                         }
                 );
+
+
         cache.put(1, 1);
         cache.put(2, 2);
         System.out.println(cache.asMap());
